c25f86b750a65ecb83a1eab47c1e5ed11b6572d4
Federico <Federico@172.22.109.254>
2019-05-21 09:03:56 +0200

    modifiche peer.

--- GUI/src/gui/frameManuale.java
+++ GUI/src/gui/frameManuale.java
@@ -14,6 +14,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
+
 /**
  *
  * @author master
@@ -23,10 +24,12 @@ public class frameManuale extends javax.swing.JFrame {
     /**
      * Creates new form frameManuale
      */
-        OpenCVFrameGrabber grabber ;
+    OpenCVFrameGrabber grabber;
+    GestioneTCP gestione;
 
     public frameManuale() {
         initComponents();
+        gestione = new GestioneTCP();
     }
 
     /**
@@ -38,43 +41,85 @@ public class frameManuale extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jButton1 = new javax.swing.JButton();
+        jButtonHome = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
+        jButtonSu = new javax.swing.JButton();
+        jButtonGiu = new javax.swing.JButton();
+        jButtonDestra = new javax.swing.JButton();
+        jButtonSinistra = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setPreferredSize(new java.awt.Dimension(737, 472));
         addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowDeactivated(java.awt.event.WindowEvent evt) {
+                formWindowDeactivated(evt);
+            }
             public void windowOpened(java.awt.event.WindowEvent evt) {
                 formWindowOpened(evt);
             }
         });
 
-        jButton1.setText("HOME");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        jButtonHome.setText("HOME");
+        jButtonHome.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                jButtonHomeActionPerformed(evt);
             }
         });
 
         jLabel1.setText("MANUALE");
 
+        jButtonSu.setText("SU");
+        jButtonSu.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonSuActionPerformed(evt);
+            }
+        });
+
+        jButtonGiu.setText("GIU");
+        jButtonGiu.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonGiuActionPerformed(evt);
+            }
+        });
+
+        jButtonDestra.setText("DESTRA");
+        jButtonDestra.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonDestraActionPerformed(evt);
+            }
+        });
+
+        jButtonSinistra.setText("SINISTRA");
+        jButtonSinistra.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonSinistraActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addContainerGap(667, Short.MAX_VALUE)
-                        .addComponent(jButton1))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(357, 357, 357)
-                        .addComponent(jLabel1)
-                        .addGap(0, 0, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addGroup(layout.createSequentialGroup()
                         .addContainerGap()
-                        .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                        .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addGap(357, 357, 357)
+                        .addComponent(jLabel1)
+                        .addGap(0, 318, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jButtonSu, javax.swing.GroupLayout.PREFERRED_SIZE, 76, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(15, 15, 15)
+                        .addComponent(jButtonGiu, javax.swing.GroupLayout.PREFERRED_SIZE, 83, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(jButtonDestra)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jButtonSinistra)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jButtonHome)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -85,31 +130,84 @@ public class frameManuale extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, 400, Short.MAX_VALUE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton1)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButtonHome)
+                    .addComponent(jButtonDestra)
+                    .addComponent(jButtonSinistra)
+                    .addComponent(jButtonGiu)
+                    .addComponent(jButtonSu))
                 .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void jButtonHomeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonHomeActionPerformed
         // BOTTONE HOME           
-        
-        Frame frameHome=new home();
+
+        Frame frameHome = new home();
         frameHome.setVisible(true);
         this.dispose();
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_jButtonHomeActionPerformed
 
     private void formWindowOpened(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowOpened
-        Thread t = new Thread(){
+        Thread t = new Thread() {
             @Override
-            public void run(){
-                startCam("rtsp://192.168.1.3:554/MediaInput/h264",jLabel2);
+            public void run() {
+                startCam("rtsp://192.168.1.4:554/MediaInput/h264", jLabel2);
+                mandaMessaggio("MODALITA,1");
             }
         };
         t.start();
     }//GEN-LAST:event_formWindowOpened
-       public void startCam(String NomeIpCam,JLabel NomeLabelIpCam){
+
+    
+    public void mandaMessaggio(String messaggio)
+    {
+        gestione.MyTCPClient("192.168.1.3" ,3333);
+       gestione.ClientManda(messaggio);
+    }
+    
+    
+    private void jButtonSuActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonSuActionPerformed
+        // TODO add your handling code here:
+        //su                    
+        mandaMessaggio("MUOVI,w");
+
+
+    }//GEN-LAST:event_jButtonSuActionPerformed
+
+    private void jButtonGiuActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonGiuActionPerformed
+        // TODO add your handling code here:
+        //giu     
+        mandaMessaggio("MUOVI,s");
+        
+        
+        
+    }//GEN-LAST:event_jButtonGiuActionPerformed
+
+    private void jButtonDestraActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonDestraActionPerformed
+        // TODO add your handling code here:        
+        //destra                    
+        mandaMessaggio("MUOVI,d");
+        
+        
+        
+    }//GEN-LAST:event_jButtonDestraActionPerformed
+
+    private void jButtonSinistraActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonSinistraActionPerformed
+        // TODO add your handling code here:
+        //sinistra
+        mandaMessaggio("MUOVI,a");
+        
+        
+        
+    }//GEN-LAST:event_jButtonSinistraActionPerformed
+
+    private void formWindowDeactivated(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowDeactivated
+        mandaMessaggio("MODALITA,0");
+    }//GEN-LAST:event_formWindowDeactivated
+    public void startCam(String NomeIpCam, JLabel NomeLabelIpCam) {
         try {
             grabber = new OpenCVFrameGrabber(NomeIpCam);
             grabber.setImageWidth(300);
@@ -121,19 +219,16 @@ public class frameManuale extends javax.swing.JFrame {
                 int HEIGHT = NomeLabelIpCam.getHeight();
                 opencv_core.IplImage resizedImage = opencv_core.IplImage.create(WIDHT, HEIGHT, originalImage.depth(), originalImage.nChannels());
                 cvResize(originalImage, resizedImage);
-                
-                
+
                 ImageIcon imageIcon = new ImageIcon(resizedImage.getBufferedImage());
                 NomeLabelIpCam.setIcon(imageIcon);
-                
+
             }
-            
-            
+
         } catch (FrameGrabber.Exception ex) {
             Logger.getLogger(index.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        
+
     }
 
     /**
@@ -172,7 +267,11 @@ public class frameManuale extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButtonDestra;
+    private javax.swing.JButton jButtonGiu;
+    private javax.swing.JButton jButtonHome;
+    private javax.swing.JButton jButtonSinistra;
+    private javax.swing.JButton jButtonSu;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     // End of variables declaration//GEN-END:variables
